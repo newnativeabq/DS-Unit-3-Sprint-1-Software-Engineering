c0f2c1623930f6726041418dcc1f4713bd0505c0
newnativeabq <42101276+newnativeabq@users.noreply.github.com>
2019-09-05 09:22:55 -0600

    updated archives package - scanner working

--- module2-oop-code-style-and-reviews/archives.py
+++ module2-oop-code-style-and-reviews/archives.py
@@ -1,55 +1,86 @@
 import os.path
-from os import path
-import os
 
-from shutil import make_archive
-
+import archive_builders
+import archive_scanners
 
 class Archive():
-	def __init__(self, path=None, output_format=None, archive_name=None, root_dir=None):
+	def __init__(self, path=None, format=None):
+		self.get_path(path)
+		self.get_format_from_path(self.path)
+
+	def get_path(self, path):
 		if path == None:
 			self.path = os.getcwd()
 		else:
-			self.path = path
-		self.format = self.get_format()
-		self.output_format = output_format
-		self.archive_name = archive_name
-		if file_exists(self.path):
-			self.assign_scanner()
-		if output_format != None:
-			self.assign_builder()
+			self.path = os.path.abspath(path)
+
+	def get_format_from_path(self, path):
+		# Set archive view type (viewing directory of files or compressed file)
+		if path_is_directory(path):
+			self.format = 'directory'
 		else:
-			self.output_format = format
+			print(parse_format_from_path(self.path))
+			self.format = parse_format_from_path(self.path)
 
-	def assign_scanner(self):
-		self.root_dir, self.scanner = 'Directory', DummyScanner()
-	def assign_builder(self):
-		self.builder = DummyBuilder()
-	def get_format(self):
-		return parse_format_from_path(self.path)
-	def set_format(self):
-		self.builder = 'TestBuilder'
+	def set_format(self, format):
+		if format_is_valid(format):
+			self.format = format
+		else:
+			print('Invalid Format')
 
-
-class DummyBuilder(Archive):
-	def __init__(self, **kwargs):
-		super().__init__(kwargs)
-	def build(self):
-		return make_archive(self.archive_name, self.format, self.root_dir)
-
-class DummyScanner(Archive):
-	def __init__(self, **kwargs):
-		super().__init__(kwargs)
 	def scan(self):
-		return os.listdir(self.path)
+		scanner = assign_scanner(path=self.path, format=self.format)
+		self.__file_list = scanner.scan()
+		print(self.__file_list)
 
+
+def path_is_directory(path):
+	return os.path.isdir(path)
 
 def parse_format_from_path(path):
-	path_as_str = str(path)
-	return path_as_str.split('.')[-1]
-
+	if os.path.isfile(path):
+		if type(path) == str:
+			path_as_str = path
+		else:
+			path_as_str = str(path)
+		suffix = path_as_str.split('.')[-1]
+	else:
+		raise TypeError
+	return suffix
 
 def file_exists(path):
-	print('checking path', path)
 	return os.path.exists(path)
 
+def format_is_valid(format):
+	builder_options = list(builder_menu.keys())
+	scanner_options = list(scanner_menu.keys())
+
+	if (format in builder_options) | (format in scanner_options):
+		return True
+	else:
+		return False
+
+def menu_lookup(format, menu):
+	return menu[format]
+
+def assign_scanner(path, format):
+	scanner_details = menu_lookup(format, scanner_menu)
+	scanner = scanner_details[0](path=path, format=scanner_details[1])
+	return scanner
+
+builder_menu = {
+	'bztar': (archive_builders.StandardBuilder, 'bztar'),
+	'gztar': (archive_builders.StandardBuilder, 'gztar'),
+	'tar': (archive_builders.StandardBuilder, 'tar'), 
+	'xztar': (archive_builders.StandardBuilder, 'xztar'),
+	'zip': (archive_builders.StandardBuilder, 'zip'),
+}
+
+scanner_menu = {
+	'bztar': (None, 'bztar'),
+	'gztar': (None, 'gztar'),
+	'tar': (None, 'tar'), 
+	'xztar': (None, 'xztar'),
+	'zip': (archive_scanners.ZipScanner, 'zip'),
+	'directory': (archive_scanners.DirectoryScanner, 'directory'),
+}
